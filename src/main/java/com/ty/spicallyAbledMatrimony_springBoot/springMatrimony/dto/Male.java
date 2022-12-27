a2ba91436599710f77dd0e4ef8f7cb49ee06c717
manoj kulli <sarupunit@gmail.com>
2022-12-27 10:37:42 +0530

    src pom.xml added

---
+++ src/main/java/com/ty/spicallyAbledMatrimony_springBoot/springMatrimony/dto/Male.java
@@ -0,0 +1,5 @@
+package com.ty.spicallyAbledMatrimony_springBoot.springMatrimony.dto;
+
+public class Male {
+
+}
---
+++ src/test/java/com/ty/spicallyAbledMatrimony_springBoot/springMatrimony/SpringMatrimonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.spicallyAbledMatrimony_springBoot.springMatrimony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMatrimonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
